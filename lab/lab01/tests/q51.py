5d252608b39f2e1c77608c2b37e711d0d073027c
seanjaffe1 <seanjaffe1@gmail.com>
2023-04-12 05:20:26 +0000

    added lab01

---
+++ lab/lab01/tests/q51.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q51',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> round(paola_distance_from_average_m, 5) == 0.072\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
