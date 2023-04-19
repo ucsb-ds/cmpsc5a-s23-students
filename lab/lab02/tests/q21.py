ec8da380b98158dac3ccae2adebfa309552ccbf4
seanjaffe1 <seanjaffe1@gmail.com>
2023-04-19 21:51:01 +0000

    small test edits

--- lab/lab02/tests/q21.py
+++ lab/lab02/tests/q21.py
@@ -2,4 +2,4 @@ OK_FORMAT = True
 
 test = {   'name': 'q21',
     'points': None,
-    'suites': [{'cases': [{'code': '>>> round(near_twenty, 8) == 19.99909998\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> np.isclose(near_twenty,19.99909997918947)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- lab/lab02/tests/q211.py
+++ lab/lab02/tests/q211.py
@@ -5,7 +5,7 @@ test = {   'name': 'q211',
     'suites': [   {   'cases': [   {   'code': '>>> # Hint: You can write the sine of 1.5*pi as:\n'
                                                '>>> #   math.sin(1.5 * math.pi)\n'
                                                '>>> import math\n'
-                                               '>>> round(sine_of_pi_over_four, 8) == 0.70710678\n'
+                                               '>>> np.isclose(sine_of_pi_over_four, 0.7071067811865475) \n'
                                                'True',
                                        'hidden': False,
                                        'locked': False}],
