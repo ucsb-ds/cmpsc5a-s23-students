ee3507fbd2077efe568d7cd0db6786c8e1ab8bad
seanjaffe1 <seanjaffe1@gmail.com>
2023-05-03 07:25:06 +0000

    lab4

---
+++ lab/lab04/tests/q1_1.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> to_percentage(0.2) == 20\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> to_percentage(0) == 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> to_percentage(1) == 100\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab04/tests/q1_4.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_4',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> num_non_vowels("apple") == 3\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> num_non_vowels("AEIOUaeiou") == 0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
