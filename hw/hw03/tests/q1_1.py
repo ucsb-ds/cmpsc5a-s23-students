1fb2cd276ec7b29dcb527e6145d75904b1f6911e
seanjaffe1 <seanjaffe1@gmail.com>
2023-05-03 07:09:28 +0000

    hw03

---
+++ hw/hw03/tests/q1_1.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_1',
+    'points': None,
+    'suites': [   {   'cases': [{'code': ">>> unemployment.select('Date', 'NEI', 'NEI-PTER').take(0).column(0).item(0) == '1994-01-01'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw03/tests/q1_5.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_5',
+    'points': None,
+    'suites': [{'cases': [{'code': ">>> by_pter.take(0).column(0).item(0) == '2009-07-01'\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
