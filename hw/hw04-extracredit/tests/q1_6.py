25cebd13c7090a1d0b3c86de6650ab026f69023b
seanjaffe1 <seanjaffe1@gmail.com>
2023-05-10 16:33:57 +0000

    hw4

---
+++ hw/hw04-extracredit/tests/q1_6.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_6',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> # Number of columns of the resulting table should be 3\n>>> menu_average.num_columns == 3\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
