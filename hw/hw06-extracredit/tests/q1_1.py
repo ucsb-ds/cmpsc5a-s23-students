7cfe00a6b8fc374055435accadd059498e716d8c
seanjaffe1 <seanjaffe1@gmail.com>
2023-05-31 05:46:41 +0000

    hw06, final

---
+++ hw/hw06-extracredit/tests/q1_1.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q1_1', 'points': None, 'suites': [{'cases': [{'code': '>>> ten_sixes in [1,2,3]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ projects/final/tests/q1_3_1.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q1_3_1', 'points': 0, 'suites': [{'cases': [{'code': '>>> ready_for_submission\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
