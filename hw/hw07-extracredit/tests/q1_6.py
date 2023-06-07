f2f1836719e99ccdff7f6ec5f1a854763fb5fd7d
seanjaffe1 <seanjaffe1@gmail.com>
2023-06-07 16:04:45 +0000

    hw7

---
+++ hw/hw07-extracredit/tests/q1_6.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_6',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> int(round(observed_statistic,2)) == 6\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
